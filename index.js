53d1c529142fb9c17b8327afb960fa66de045627
Michael Klein <michael@firstiwaslike.com>
2017-07-04 11:33:25 +0200

    Initial spike

--- index.js
+++ index.js
@@ -1,6 +1,81 @@
 /* eslint-env node */
 'use strict';
 
+const fs         = require('fs');
+const path       = require('path');
+const AdmZip     = require('adm-zip');
+const BasePlugin = require('ember-cli-deploy-plugin');
+const rimraf     = require('rimraf');
+const RSVP       = require('rsvp');
+
 module.exports = {
-  name: 'ember-cli-deploy-fastboot-app-server'
+  name: 'ember-cli-deploy-fastboot-app-server',
+
+  createDeployPlugin: function(options) {
+    let DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        distDir: function(context) {
+          return context.distDir;
+        },
+
+        fastbootDistDir: 'tmp/fastboot-deploy',
+
+        revisionKey: function(context) {
+          let revisionKey = context.revisionData && context.revisionData.revisionKey;
+          return context.commandOptions.revision || revisionKey;
+        },
+
+        downloaderManifestContent: function() {
+          return function(bucket, key) {
+            return `
+              {
+                "bucket": "${bucket}",
+                "key": "${key}"
+              }
+            `;
+          };
+        }
+      },
+
+      setup: function() {
+        let downloaderManifestContent = this.readConfig('downloaderManifestContent');
+
+        return { downloaderManifestContent };
+      },
+
+      willBuild: function() {
+        let fastbootDistDir = this.readConfig('fastbootDistDir');
+
+        rimraf.sync(fastbootDistDir);
+
+        return RSVP.resolve();
+      },
+
+      prepare: function() {
+        let distDir         = this.readConfig('distDir');
+        let revisionKey     = this.readConfig('revisionKey');
+        let fastbootDistDir = this.readConfig('fastbootDistDir');
+
+        if (!fs.existsSync(fastbootDistDir)) {
+          fs.mkdirSync(fastbootDistDir);
+        }
+
+        let archiveName = 'dist-'+revisionKey+'.zip';
+        let archivePath = path.join(fastbootDistDir, archiveName);
+
+        let zip = new AdmZip();
+        zip.addLocalFolder(distDir);
+        zip.writeZip(archivePath);
+
+        return {
+          fastbootArchiveName: archiveName,
+          fastbootArchivePath: archivePath
+        };
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
---
+++ tests/index-test.js
@@ -0,0 +1,157 @@
+/* eslint-env node */
+'use strict';
+
+const fs     = require('fs');
+const AdmZip = require('adm-zip');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const assert = require('./helpers/assert');
+
+var stubProject = {
+  name: function() {
+    return 'my-project';
+  }
+};
+
+describe('fastboot-app-server plugin', function() {
+  var subject, plugin, mockUI, context;
+
+  before(function() {
+    subject = require('../index');
+  });
+
+  beforeEach(function() {
+    mockUI = {
+      verbose: true,
+      messages: [],
+      write: function() { },
+      writeLine: function(message) {
+        this.messages.push(message);
+      }
+    };
+
+    plugin = subject.createDeployPlugin({
+      name: 'fastboot-app-server'
+    });
+
+    context = {
+      ui: mockUI,
+
+      project: stubProject,
+
+      commandOptions: {},
+
+      config: {
+        'fastboot-app-server': {
+        }
+      }
+    }
+  });
+
+  it('has a name', function() {
+    assert.equal(plugin.name, 'fastboot-app-server');
+  });
+
+  describe('hooks', function() {
+    beforeEach(function() {
+      plugin.beforeHook(context);
+      plugin.configure(context);
+    });
+
+    it('implements the correct hooks', function() {
+      assert.ok(plugin.configure);
+      assert.ok(plugin.setup);
+      assert.ok(plugin.willBuild);
+      assert.ok(plugin.prepare);
+    });
+
+    describe('#setup', function() {
+      it('adds a function to the deploy context that can be used to write a fastboot-app-server manifest', function() {
+        let dataToMergeIntoDeployContext = plugin.setup(context);
+
+        let downloaderManifestContent = dataToMergeIntoDeployContext.downloaderManifestContent;
+
+        let manifestContent = downloaderManifestContent('bucket-name', 'revision-key');
+
+        let expectedJSON = {
+          bucket: 'bucket-name',
+          key: 'revision-key'
+        };
+
+        assert.deepEqual(JSON.parse(manifestContent), expectedJSON);
+      });
+    });
+
+    describe('#willBuild', function() {
+      it('doesn not fail when `fastbootDistDir` does not exist', function() {
+        plugin.willBuild(context);
+
+        assert.notOk(fs.existsSync('tmp/fastboot-deploy'));
+      });
+
+      it('cleans up `fastbootDistDir` if it exists', function() {
+        mkdirp.sync('tmp/fastboot-deploy');
+
+        assert.ok(fs.existsSync('tmp/fastboot-deploy'));
+
+        plugin.willBuild(context);
+
+        assert.notOk(fs.existsSync('tmp/fastboot-deploy'));
+      });
+
+      it('cleans up `fastbootDistDir` if it exists and another DIR is configured than the default', function() {
+        let FASTBOOT_DIST_DIR = 'tmp/lolbar';
+
+        context.config['fastboot-app-server'].fastbootDistDir = FASTBOOT_DIST_DIR;
+
+        mkdirp.sync(FASTBOOT_DIST_DIR);
+
+        assert.ok(fs.existsSync(FASTBOOT_DIST_DIR));
+
+        plugin.willBuild(context);
+
+        assert.notOk(fs.existsSync(FASTBOOT_DIST_DIR));
+      });
+    });
+
+    describe('#prepare', function() {
+      beforeEach(function() {
+        let DIST_DIR = 'tmp/deploy-dist'
+
+        rimraf.sync('tmp');
+
+        context.distDir = DIST_DIR;
+        context.revisionData = {
+          revisionKey: '1234'
+        };
+
+        mkdirp.sync(DIST_DIR);
+        fs.writeFileSync(`${DIST_DIR}/deploy.txt`, 'deployment');
+      });
+
+      it('zips the contents of `distDir` and writes them to `fastbootDistDir` as a zip tagged by `revisionKey`', function() {
+        plugin.prepare(context);
+
+        assert.ok(fs.existsSync('tmp/fastboot-deploy/dist-1234.zip'));
+      });
+
+      it('zips the content of distDir as expected', function() {
+        plugin.prepare(context);
+
+        let zip = new AdmZip('tmp/fastboot-deploy/dist-1234.zip');
+        zip.extractAllTo('tmp/fastboot-deploy', true);
+
+        let deployText = fs.readFileSync('tmp/fastboot-deploy/deploy.txt')
+
+        assert.equal(deployText, 'deployment');
+      });
+
+      it('adds fastbootArchiveName and fastbootArchivePath info to the deplyoment context', function() {
+        let info = plugin.prepare(context);
+
+        assert.equal(info.fastbootArchiveName, 'dist-1234.zip');
+        assert.equal(info.fastbootArchivePath, 'tmp/fastboot-deploy/dist-1234.zip');
+      });
+    });
+  });
+});
