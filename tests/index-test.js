6b8f97b85a1eb5816e0cd0708a5b2289407b998b
Steve <Steve.Hummingbird@yandex.com>
2020-05-18 14:35:44 +0200

    adds option ignoreFiles, which allows to ignore certain files (#8)
    * allow specify files to ignore
    
    * update message
    
    * add assert msg

--- tests/index-test.js
+++ tests/index-test.js
@@ -134,6 +134,9 @@ describe('fastboot-app-server plugin', function() {
 
         mkdirp.sync(DIST_DIR);
         fs.writeFileSync(`${DIST_DIR}/deploy.txt`, 'deployment');
+        mkdirp.sync(`${DIST_DIR}/assets`);
+        fs.writeFileSync(`${DIST_DIR}/assets/app.js`, 'deployment');
+        fs.writeFileSync(`${DIST_DIR}/assets/app.map`, 'deployment');
       });
 
       it('zips the contents of `distDir` and writes them to `fastbootDistDir` as a zip tagged by `revisionKey`', function() {
@@ -152,6 +155,59 @@ describe('fastboot-app-server plugin', function() {
             let deployText = fs.readFileSync('tmp/fastboot-deploy/dist/deploy.txt')
 
             assert.equal(deployText, 'deployment');
+            assert.ok(fs.existsSync('tmp/fastboot-deploy/dist/assets/app.js'));
+            assert.ok(fs.existsSync('tmp/fastboot-deploy/dist/assets/app.map'));
+          })
+      });
+
+      it('adds fastbootArchiveName and fastbootArchivePath info to the deplyoment context', function() {
+        return plugin.didPrepare(context)
+          .then((info) => {
+            assert.equal(info.fastbootArchiveName, 'dist-1234.zip');
+            assert.equal(info.fastbootArchivePath, 'tmp/fastboot-deploy/dist-1234.zip');
+          });
+      });
+    });
+
+    describe('#didPrepare/globPattern - ignore all .map files', function() {
+      beforeEach(function() {
+        context.config['fastboot-app-server'].ignoreFiles = "**/*.map";
+        plugin.configure(context);
+
+        let DIST_DIR = 'tmp/deploy-dist'
+        rimraf.sync('tmp');
+
+        context.fastbootArchivePrefix = 'dist-';
+        context.distDir = DIST_DIR;
+        context.revisionData = {
+          revisionKey: '1234'
+        };
+
+        mkdirp.sync(DIST_DIR);
+        fs.writeFileSync(`${DIST_DIR}/deploy.txt`, 'deployment');
+        mkdirp.sync(`${DIST_DIR}/assets`);
+        fs.writeFileSync(`${DIST_DIR}/assets/app.js`, 'deployment');
+        fs.writeFileSync(`${DIST_DIR}/assets/app.map`, 'deployment');
+      });
+
+      it('zips the contents of `distDir` and writes them to `fastbootDistDir` as a zip tagged by `revisionKey`', function() {
+        return plugin.didPrepare(context)
+          .then(() => {
+            assert.ok(fs.existsSync('tmp/fastboot-deploy/dist-1234.zip'));
+          });
+      });
+
+      it('compressed zip should not contain any .map files - all other files should be included', function() {
+        return plugin.didPrepare(context)
+          .then(() => {
+            return decompress('tmp/fastboot-deploy/dist-1234.zip', 'tmp/fastboot-deploy');
+          })
+          .then(() => {
+            let deployText = fs.readFileSync('tmp/fastboot-deploy/dist/deploy.txt')
+
+            assert.equal(deployText, 'deployment');
+            assert.ok(fs.existsSync('tmp/fastboot-deploy/dist/assets/app.js'), 'app.js is included in zipped files');
+            assert.notOk(fs.existsSync('tmp/fastboot-deploy/dist/assets/app.map'), 'app.map is not included in zipped files');
           })
       });
 
